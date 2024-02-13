66d8339a7796ae386e48777c86dfa1779c91e102
Anthonin239 <159912857+Anthonin239@users.noreply.github.com>
2024-02-13 16:21:01 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -14,6 +14,10 @@ def MaPremiereAPI():
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
+
+@app.route("/histogramme/")
+def mongraphique2():
+    return render_template("histogramme.html")
   
 @app.route('/')
 def hello_world():
@@ -22,18 +26,6 @@ def hello_world():
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
-    raw_content = response.read()
-    json_content = json.loads(raw_content.decode('utf-8'))
-    results = []
-    for list_element in json_content.get('list', []):
-        dt_value = list_element.get('dt')
-        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
-        results.append({'Jour': dt_value, 'temp': temp_day_value})
-    return jsonify(results=results)
-
-@app.route('/histogramme/')
-def meteo2():
-    response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=48.8566&lon=2.3522&appid=xxx')
     raw_content = response.read()
     json_content = json.loads(raw_content.decode('utf-8'))
     results = []
